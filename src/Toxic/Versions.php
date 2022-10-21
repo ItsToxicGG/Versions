13c63f53e73494fb8adde112ea2267d9fbcdf1d3
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-10-21 19:19:27 +0200

    Create Versions.php

---
+++ src/Toxic/Versions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Toxic;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Versions extends PluginBase;
+ 
+   public function onEnable(): void{ // ??}
+}
